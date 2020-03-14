a449accb4bff653f41f9c2940626c07c583d188a
vegardskeie <60470254+vegardskeie@users.noreply.github.com>
2020-03-14 17:49:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+//Question 1
+var petArray = ["cat", "cow", "dog"];
+
+//Question 2
+console.log("cow");
+
+//Question 3
+petArray.push("sheep");
+
+//Question 4
+console.log(petArray.length);
+
+//Question 5
+var catObject = {
+  name: "ole",
+  color: "black",
+  age: 5
+};
+
+//Question 6
+console.log(catObject.age);
+
+//Question 7
+var catArray = [
+  {
+    name: "ole",
+    color: "black",
+    age: 5
+  }
+];
+
+//Question 8
+for (var i = 0; i < catArray.length; i++) {
+  console.log(catArray[i].name);
+
+  console.log(catArray[i].color);
+
+  console.log(catArray[i].age);
+}
+
+//Question 9
+function logToConsole(skeie) {
+  console.log("skeie");
+}
+
+//Question 10
+logToConsole("donkey");
